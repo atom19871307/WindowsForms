65d2fe03752021027b5f2a21eb6e73c543f36631
Atom Mayilyan <mayilyan.atom@mail.ru>
2026-01-27 21:26:28 +0300

    Add project files.

---
+++ Clock/MainForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void timer_Tick(object sender, EventArgs e)
+		{
+			labelTime.Text = DateTime.Now.ToString
+				(
+				"hh:mm:ss tt",
+				System.Globalization.CultureInfo.InvariantCulture
+				);
+			if (checkBoxShowDate.Checked)
+				labelTime.Text += $"\n{DateTime.Now.ToString("yyyy.MM.dd")}";
+		}
+	}
+}
